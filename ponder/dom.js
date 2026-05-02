a7a0c55dc49532dc442c31c423b0a7fbee9e2ef5
brycekt887 <brycekt@byui.edu>
2026-05-01 20:49:54 -0600

    image fix??

--- ponder/dom.js
+++ ponder/dom.js
@@ -23,7 +23,7 @@ para.classList.add('background');
 //document.querySelector('body').classList.add('background');
 
 const image = document.querySelector('img');
-image.setAttribute('src', 'images/working pic.jpg');
+image.setAttribute('src', 'images/working-pic.jpg');
 
 
 let selectElem = document.getElementById('webdevlist');
